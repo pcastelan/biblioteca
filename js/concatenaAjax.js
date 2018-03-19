cdde3c58d46b48002e8a3fb7d4f4721759758727
Paula Castelan <paulacastelanf@gmail.com>
2018-03-19 16:59:11 -0300

    Update concatenaAjax.js

--- js/concatenaAjax.js
+++ js/concatenaAjax.js
@@ -4,40 +4,35 @@
  *
  *   possível aumentar o numero de chamadas, contanto que os jsons tenham sempre a mesma estrutura
 */
-
-function multiplosJSONS(){
  
-  var chamada1 = $.ajax({
-    url: "ajax/arquivo1.json",
-    type: "GET",
-    dataType: "json",
-    headers: {
-      Accept: "application/json"
-    } 
-  });
+var chamada1 = $.ajax({
+  url: "ajax/arquivo1.json",
+  type: "GET",
+  dataType: "json",
+  headers: {
+    Accept: "application/json"
+  } 
+});
 
-  var chamada2 = $.ajax({
-    url: "ajax/arquivo2.json",
-    type: "GET",
-    dataType: "json",
-    headers: {
-      Accept: "application/json"
-    } 
-  });
-  
-  //depois que as chamadas estiverem finalizadas
-  $.when(chamada1, chamada2).done(function(resultado1, resultado2){
-    //cada chamada tem como retorno um array com [ data, status, jqXHR ]
-    
-    //o resto do código deve ser adequado a estrutura dos objetos json
-    var data = resultado1[0];
+var chamada2 = $.ajax({
+  url: "ajax/arquivo2.json",
+  type: "GET",
+  dataType: "json",
+  headers: {
+    Accept: "application/json"
+  } 
+});
 
-    for (var i in resultado2[0]){
-      data.push(resultado2[0][i]);
-    }
-    
-    return data;
-  });
- 
-  
-}
+//depois que as chamadas estiverem finalizadas
+$.when(chamada1, chamada2).done(function(resultado1, resultado2){
+  //cada chamada tem como retorno um array com [ data, status, jqXHR ]
+
+  //o resto do código deve ser adequado a estrutura dos objetos json
+  var data = resultado1[0];
+
+  for (var i in resultado2[0]){
+    data.push(resultado2[0][i]);
+  }
+
+  return data;
+});
